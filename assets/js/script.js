334b55a1f35f923cc8cbb7d0c950ea783b976adf
Alex <lex.courtney18@gmail.com>
2022-06-12 18:48:06 -0500

    stringify local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,6 +62,8 @@ var completeEditTask = function(taskName, taskType, taskId) {
 
     formEl.removeAttribute("data-task-id");
     document.querySelector("#save-task").textContent = "Add Task";
+
+    saveTasks();
 };
 
 var createTaskEl = function (taskDataObj) {
@@ -90,8 +92,7 @@ var createTaskEl = function (taskDataObj) {
     // increase task counter for next unique id
     taskIdCounter++;
 
-    console.log(taskDataObj);
-    console.log(taskDataObj.status);
+    saveTasks();
 
 };
 
@@ -173,6 +174,8 @@ var deleteTask = function(taskId) {
 
 // reassign tasks array to be the same as updatedTaskArr
     tasks = updatedTaskArr;
+
+    saveTasks();
 };
 
 var editTask = function(taskId) {
@@ -221,8 +224,13 @@ var taskStatusChangeHandler = function(event) {
             tasks[i].status = statusValue;
         }
     }
-    console.log(tasks);
+
+    saveTasks();
 };
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
